fe430820b20af880bd9cda3021f6118758c37155
Rajani-Navoda <rajaninavoda@gmail.com>
2024-10-30 20:20:41 +0530

    add responsiveness

--- my_portfollio/src/app/about-me/about-me.component.ts
+++ my_portfollio/src/app/about-me/about-me.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { WorkExperienceEducationComponent } from "../work-experience-education/work-experience-education.component";
-import { ProjectsComponent } from "../projects/projects.component";
-import { EducationComponent } from "../education/education.component";
-import { RouterModule } from '@angular/router';
-import { SkillsComponent } from "../skills/skills.component";
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [WorkExperienceEducationComponent, ProjectsComponent, EducationComponent, RouterModule, SkillsComponent],
+  imports: [],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
